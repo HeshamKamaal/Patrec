0f65a9e4ec3af408ab274ba05428763a0f60c608
Hesham Kamal <heshamkamal@Heshams-MacBook-Air.local>
2023-07-14 20:44:43 +0300

    resgister

---
+++ Patrec/RegisterViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterViewController.swift
+//  Patrec
+//
+//  Created by Hesham Kamal on 14/07/2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+
+    
+    
+}
